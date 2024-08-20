c74e9a7876043fc63284e035a3cb7f983408e9e6
Genaro12ofi <66640217+Genaro12ofi@users.noreply.github.com>
2024-08-20 11:31:10 -0600

    Update gasolinera.js

--- gasolinera.js
+++ gasolinera.js
@@ -10,5 +10,16 @@ var gasolineras = [
         ubicacion: "Autopista Libramiento KM 19.5, Laa trojes, Temoaya, Méx.",
         preciodiesel: "$24.59"
     }
+     {
+        id: 2,
+        nombre: "Ranitaw",
+        latitud: 19.3021175,
+        longitud: -99.623011,
+        enlace: "https://maps.app.goo.gl/GJoG2mwpBgAF8p1L9",
+        precioroja: "$3.5",
+        precioverde: "$22.50",
+        ubicacion: "Autopista Libramiento KM 19.5, Laa trojes, Temoaya, Méx.",
+        preciodiesel: "$24.59"
+    }
     // Puedes agregar más gasolineras aquí siguiendo el mismo formato
 ];
