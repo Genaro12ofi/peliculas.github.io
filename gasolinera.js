2fdd30b4aeb5f6daab7e806d0bf3ba46f5c7f962
Genaro12ofi <66640217+Genaro12ofi@users.noreply.github.com>
2024-08-20 11:13:22 -0600

    Add files via upload

---
+++ gasolinera.js
@@ -0,0 +1,14 @@
+var gasolineras = [
+    {
+        id: 1,
+        nombre: "Ranita",
+        latitud: 19.3021175,
+        longitud: -99.623011,
+        enlace: "https://maps.app.goo.gl/GJoG2mwpBgAF8p1L9",
+        precioroja: "$3.5",
+        precioverde: "$22.50",
+        ubicacion: "Autopista Libramiento KM 19.5, Laa trojes, Temoaya, Méx.",
+        preciodiesel: "$24.59"
+    }
+    // Puedes agregar más gasolineras aquí siguiendo el mismo formato
+];
