ea34fb9771b8c654188e4f4d68dbfa51d31bac89
Genaro12ofi <66640217+Genaro12ofi@users.noreply.github.com>
2024-09-12 07:41:09 -0600

    Update gasolinera.js

--- gasolinera.js
+++ gasolinera.js
@@ -5,10 +5,10 @@ var gasolineras = [
         latitud: 19.3021175,
         longitud: -99.623011,
         enlace: "https://maps.app.goo.gl/GJoG2mwpBgAF8p1L9",
-        precioroja: "$3.5",
+        precioroja: "$22.00",
         precioverde: "$22.50",
         ubicacion: "Autopista Libramiento KM 19.5, Laa trojes, Temoaya, Méx.",
-        preciodiesel: "$24.59"
+        preciodiesel: "$22.19"
     },
      {
         id: 2,
@@ -16,10 +16,10 @@ var gasolineras = [
         latitud: 19.310467180432596,
         longitud: -99.63436368852854,
         enlace: "https://maps.app.goo.gl/GJoG2mwpBgAF8p1L9",
-        precioroja: "$3.5",
+        precioroja: "$22.45",
         precioverde: "$22.50",
         ubicacion: "Autopista Libramiento KM 19.5, Laa trojes, Temoaya, Méx.",
-        preciodiesel: "$24.59"
+        preciodiesel: "$22.39"
     },
 
          {
@@ -28,10 +28,10 @@ var gasolineras = [
         latitud: 19.36965903495495,
         longitud: -99.6954520046711,
         enlace: "https://maps.app.goo.gl/GJoG2mwpBgAF8p1L9",
-        precioroja: "$3.5",
+        precioroja: "$22.65",
         precioverde: "$22.50",
         ubicacion: "Autopista Libramiento KM 19.5, Laa trojes, Temoaya, Méx.",
-        preciodiesel: "$24.59"
+        preciodiesel: "$22.59"
     }
     // Puedes agregar más gasolineras aquí siguiendo el mismo formato
 ];
